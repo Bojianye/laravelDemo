7ac372e2e60c62d5ae34a7060fb7d7d5da45deb0
RobertBo <bojianye@gmail.com>
2018-10-05 01:06:35 +1300

    fixed port

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Middleware;
 use Closure;
+
 class EnableCrossRequestMiddleware
 {
     /**
@@ -15,7 +16,7 @@ class EnableCrossRequestMiddleware
         $response = $next($request);
         $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
         $allow_origin = [
-            'http://localhost:8000',
+            'http://localhost:3000',
             '*'
         ];
         if (in_array($origin, $allow_origin)) {
